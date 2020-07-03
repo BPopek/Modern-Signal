941a954212911050c8b1208c60062e6c6c2915e4
BPopek <betsypopek@gmail.com>
2020-07-03 11:28:33 -0600

    nitpicky change all quotes

--- nasfa/app.js
+++ nasfa/app.js
@@ -1,27 +1,21 @@
 
 function showHideRegs() {
-    // let element = document.getElementById("ask-regs-all");
-    // element.classList.remove('hide-all-regs');
+    let element = document.getElementById('ask-regs-all');
+    let iconElements = document.getElementById('ask-regs-header');
 
-    // toggleClass(document.getElementById("ask-regs-all"), ['toggle-show-all-regs', 'toggle-hide-all-regs']);
-
-    let element = document.getElementById("ask-regs-all");
-    let iconElements = document.getElementById("ask-regs-header");
-
-
-    if(element.className == "regs hide-all-regs") {
+    if(element.className == 'regs hide-all-regs') {
         element.classList.remove('hide-all-regs');
         element.classList.add('toggle-show-all-regs'); 
         iconElements.classList.toggle('expanded-header'); 
         iconElements.classList.toggle('collapsed-header'); 
     }
-    else if(element.className == "regs toggle-show-all-regs") {
+    else if(element.className == 'regs toggle-show-all-regs') {
         element.classList.remove('toggle-show-all-regs');
         element.classList.add('toggle-hide-all-regs');   
         iconElements.classList.toggle('expanded-header'); 
         iconElements.classList.toggle('collapsed-header'); 
     }
-    else if(element.className == "regs toggle-hide-all-regs") {
+    else if(element.className == 'regs toggle-hide-all-regs') {
         element.classList.remove('toggle-hide-all-regs');   
         element.classList.add('toggle-show-all-regs');   
         iconElements.classList.toggle('expanded-header'); 
