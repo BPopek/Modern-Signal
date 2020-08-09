0c2c1af6773ada9f1fb557c9c31e28f1db295065
BPopek <betsypopek@gmail.com>
2020-08-09 16:38:23 -0600

    most changes

--- nasfaa/app.js
+++ nasfaa/app.js
@@ -2,22 +2,30 @@
 function showHideRegs() {
     let element = document.getElementById('ask-regs-all');
     let iconElements = document.getElementById('ask-regs-header');
+    let accordian = document.getElementById('ask-regs-accordion');
+
 
     if(element.className == 'regs hide-all-regs') {
         element.classList.remove('hide-all-regs');
         element.classList.add('toggle-show-all-regs'); 
+        accordian.classList.remove('accordian-hide');
+        accordian.classList.add('accordian-show'); 
         iconElements.classList.toggle('expanded-header'); 
         iconElements.classList.toggle('collapsed-header'); 
     }
     else if(element.className == 'regs toggle-show-all-regs') {
         element.classList.remove('toggle-show-all-regs');
-        element.classList.add('toggle-hide-all-regs');   
+        element.classList.add('toggle-hide-all-regs');  
+        accordian.classList.add('accordian-hide');
+        accordian.classList.remove('accordian-show');  
         iconElements.classList.toggle('expanded-header'); 
         iconElements.classList.toggle('collapsed-header'); 
     }
     else if(element.className == 'regs toggle-hide-all-regs') {
         element.classList.remove('toggle-hide-all-regs');   
         element.classList.add('toggle-show-all-regs');   
+        accordian.classList.remove('accordian-hide');
+        accordian.classList.add('accordian-show'); 
         iconElements.classList.toggle('expanded-header'); 
         iconElements.classList.toggle('collapsed-header'); 
     }
