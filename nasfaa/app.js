9b54fd333b9e4a328c91326f07b90e5020f179eb
BPopek <betsypopek@gmail.com>
2020-08-17 14:21:59 -0600

    using session storage to hide accordian after first view

--- nasfaa/app.js
+++ nasfaa/app.js
@@ -1,23 +1,41 @@
+let element = document.getElementById('ask-regs-all');
+let iconElements = document.getElementById('ask-regs-header');
+let accordian = document.getElementById('ask-regs-accordion');
+
+if (sessionStorage.getItem('show_banner') === '1'){
+    element.classList.add('hide-all-regs');
+
+}
+if (sessionStorage.getItem('show_banner') === null){
+    sessionStorage.setItem('show_banner', '1');
+    element.classList.add('toggle-show-all-regs');
+    iconElements.classList.add('expanded-header'); 
+    iconElements.classList.remove('collapsed-header'); 
+    accordian.classList.add('accordian-show');
+}
 
 function showHideRegs() {
-    let element = document.getElementById('ask-regs-all');
-    let iconElements = document.getElementById('ask-regs-header');
-    let accordian = document.getElementById('ask-regs-accordion');
-
-
     if(element.className == 'regs') {
         element.classList.add('toggle-hide-all-regs');
         element.classList.remove('toggle-show-all-regs'); 
-        iconElements.classList.toggle('expanded-header'); 
-        iconElements.classList.toggle('collapsed-header'); 
+        iconElements.classList.add('expanded-header'); 
+        iconElements.classList.remove('collapsed-header'); 
         accordian.classList.add('accordian-hide');
         accordian.classList.remove('accordian-show'); 
+    }
+    else if(element.className == 'regs hide-all-regs') {
+        element.classList.add('toggle-show-all-regs');
+        element.classList.remove('hide-all-regs');  
+        iconElements.classList.remove('expanded-header'); 
+        iconElements.classList.add('collapsed-header'); 
+        accordian.classList.remove('accordian-hide');
+        accordian.classList.add('accordian-show'); 
     }
     else if(element.className == 'regs toggle-hide-all-regs') {
         element.classList.add('toggle-show-all-regs');
         element.classList.remove('toggle-hide-all-regs');  
-        iconElements.classList.toggle('expanded-header'); 
-        iconElements.classList.toggle('collapsed-header'); 
+        iconElements.classList.add('expanded-header'); 
+        iconElements.classList.remove('collapsed-header'); 
         accordian.classList.remove('accordian-hide');
         accordian.classList.add('accordian-show'); 
     }
@@ -26,15 +44,7 @@ function showHideRegs() {
         element.classList.remove('toggle-show-all-regs');   
         accordian.classList.add('accordian-hide');
         accordian.classList.remove('accordian-show'); 
-        iconElements.classList.toggle('expanded-header'); 
-        iconElements.classList.toggle('collapsed-header'); 
+        iconElements.classList.remove('expanded-header'); 
+        iconElements.classList.add('collapsed-header'); 
     }
 }
-
-// $(document).ready(function() {
-//     let childWidth = $('.category-tag').height()
-//     $('.tag-right').width(childWidth);
-
-//     let childHeight = $('.category-tag').height()
-//     $('.tag-right').height(childHeight);
-// })
